c0811d6f6e6645cb0745471adf0b05c62ffa05d0
Oleksandr Bozhko <Oleksandr_Bozhko@epam.com>
2023-06-02 13:43:03 +0300

    Refactored batch

--- src/main/java/org/perf/httpVsDbCompare/service/HttpService.java
+++ src/main/java/org/perf/httpVsDbCompare/service/HttpService.java
@@ -12,6 +12,9 @@ public class HttpService {
 	
 	private static final Logger LOGGER = Logger.getLogger(HttpService.class.getName());
 	
+	@Value("${http.batch.size}")
+	private Integer batchSize;
+	
 	@Value("${tenant.id}")
 	private String tenant;
 	
@@ -38,8 +41,11 @@ public class HttpService {
 		var ids = pgClient.getMarcIds(tenant, numRecords);
 		LOGGER.log(Level.INFO, "Started HTTP batch {0} records", numRecords);
 		long startTime = System.nanoTime();
-		var marc = httpClient.getMarcsByIds(ids);
-		LOGGER.log(Level.FINE, "Marc record batch: {0}", marc);
+		for (int i = 0; i < ids.size(); i += batchSize) {
+			var toIndex = i + batchSize;
+			var marc = httpClient.getMarcsByIds(ids.subList(i, toIndex >= ids.size() ? ids.size() - 1 : toIndex));
+			LOGGER.log(Level.FINE, "Marc record batch: {0}", marc);
+		}
 		var res = String.valueOf((System.nanoTime() - startTime) / 1_000_000_000);
 		LOGGER.log(Level.INFO, "Ended HTTP batch {0} records", numRecords);
 		return res;
